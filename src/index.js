c1e13701a4a3a29ca06a60d299b394adabd4cb4c
Mahao05 <prudencemolefe93@gmail.com>
2024-03-04 19:31:33 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ function formatDay(timestamp) {
   let day = new Date(timestamp * 1000);
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  return days[date.getDate];
+  return days[date.getDay()];
 }
 
 function getForecast(city) {
